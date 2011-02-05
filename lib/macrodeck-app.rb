2e07143774e24aca153cc8f5413f3bcfaa42b556
Keith Gable <ziggy@ignition-project.com>
2011-02-04 23:55:53 -0600

    Make a test app which does stuff

--- lib/macrodeck-app.rb
+++ lib/macrodeck-app.rb
@@ -1,9 +1,18 @@
 require 'sinatra/base'
+require 'erb'
 
 module MacroDeck
 	class App < Sinatra::Base
+		helpers do
+			include Rack::Utils
+			alias_method :h, :escape_html
+		end
+
+		set :public, File.join(File.dirname(__FILE__), "..", "public")
+
 		get '/' do
-			'Test'
+			@data_objects = DataObjectDefinition.all
+			erb :index
 		end
 	end
 end
