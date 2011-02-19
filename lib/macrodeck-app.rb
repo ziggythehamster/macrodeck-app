6c283dfdfed182337cddd4f69f34bf9ddbf8544e
Keith Gable <ziggy@ignition-project.com>
2011-02-18 22:08:32 -0600

    Check that we're instiantiating a DataObject and clean up the index method

--- lib/macrodeck-app.rb
+++ lib/macrodeck-app.rb
@@ -13,12 +13,17 @@ module MacroDeck
 
 			# If there ends up being a lot of these helpers, I need to move them to MacroDeck::AppHelpers or something.
 
-			# Returns either nil, or the platform object requested.
+			# Returns either nil, or the platform object requested (unless it's not a platform object)
 			# So basically get_platform_object("places").get("ID") or whatever.
 			def get_platform_object(str)
 				# Attempt a lookup like places (or place) => Place
 				begin
-					return str.pluralize.classify.constantize
+					obj = str.pluralize.classify.constantize
+					if obj.superclass == ::DataObject
+						return obj
+					else
+						return nil
+					end
 				rescue NameError
 					# Other situations (can't think of any) go here
 					return nil
@@ -34,9 +39,12 @@ module MacroDeck
 		end
 
 		get '/:object_type/?' do
-			if get_platform_object(params[:object_type])
+			@object = get_platform_object(params[:object_type])
+
+			if !@object.nil?
 				# FIXME: Probably a bad idea to load ALL objects, right? :)
-				erb :index, :layout => MD_CFG.layout.to_sym, :locals => { :objects => get_platform_object(params[:object_type]).all }
+				@objects = @object.all
+				erb :index, :layout => MD_CFG.layout.to_sym, :locals => { :objects => @objects }
 			else
 				not_found
 			end
