9b6a43c654ce06fbad9d8d44ba1c4707a72fed25
Keith Gable <ziggy@ignition-project.com>
2011-03-12 19:57:45 -0600

    Switch to passing dataobjects in instead of values, to allow behaviors to work differently if there are different attributes.

--- lib/macrodeck-behavior.rb
+++ lib/macrodeck-behavior.rb
@@ -3,14 +3,14 @@ require "behaviors/title_behavior"
 
 module MacroDeck
 	class Behavior
-		# Pass in the field value.
-		def initialize(value)
-			@value = value
+		# Pass in the data object.
+		def initialize(data_object)
+			@data_object = data_object
 		end
 
 		# Default to_html method. Override this in subclasses.
 		def to_html
-			value.to_s
+			""
 		end
 	end
 end
