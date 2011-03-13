fcd91f110024c7546174012b4f59a3ceaee8b2b6
Keith Gable <ziggy@ignition-project.com>
2011-03-12 20:50:57 -0600

    If abbreviation isn't available, accessing it as a method fails. This does not.

--- lib/behaviors/title_behavior.rb
+++ lib/behaviors/title_behavior.rb
@@ -2,7 +2,7 @@ module MacroDeck
 	# Renders a title as a heading.
 	class TitleBehavior < Behavior
 		def to_html
-			if @data_object.abbreviation
+			if @data_object[:abbreviation]
 				return "<h2>#{@data_object.title} (#{@data_object.abbreviation})</h2>"
 			else
 				return "<h2>#{@data_object.title}</h2>"
