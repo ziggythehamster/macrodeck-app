4dcfb7e4ddb826b8e43050f027fbe2ab335a7ea9
Keith Gable <ziggy@ignition-project.com>
2011-03-14 21:42:36 -0500

    HTML escape phone numbers

--- lib/behaviors/phone_number_behavior.rb
+++ lib/behaviors/phone_number_behavior.rb
@@ -2,7 +2,7 @@ module MacroDeck
 	# Renders a phone number.
 	class PhoneNumberBehavior < Behavior
 		def to_html
-			"<p><a href=\"tel:#{@data_object.phone_number}\">#{@data_object.phone_number}</a></p>"
+			"<p><a href=\"tel:#{Rack::Utils.escape_html(@data_object.phone_number)}\">#{Rack::Utils.escape_html(@data_object.phone_number)}</a></p>"
 		end
 	end
 end
