3e69c3687f7359c1470881c1a8d192c941689561
Keith Gable <ziggy@ignition-project.com>
2011-03-14 21:50:21 -0500

    Add a Geo behavior... Hope it works.

---
+++ lib/behaviors/geo_behavior.rb
@@ -0,0 +1,8 @@
+module MacroDeck
+	# Renders a geolocation as a Google Maps static map
+	class GeoBehavior < Behavior
+		def to_html
+			"<p><img src=\"http://maps.google.com/maps/api/staticmap?size=300x300&amp;markers=#{Rack::Utils.escape_html(@data_object.lat)},#{Rack::Utils.escape_html(@data_object.lng)}&amp;maptype=roadmap&amp;sensor=false\" alt=\"Map of #{Rack::Utils.escape_html(@data_object.title)}\" /></p>"
+		end
+	end
+end
