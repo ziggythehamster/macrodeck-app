d25a873f94e5cd87b8b090348d8ca8dd8a05128d
Keith Gable <ziggy@ignition-project.com>
2011-03-14 21:42:02 -0500

    HTML escape descriptions

--- lib/behaviors/description_behavior.rb
+++ lib/behaviors/description_behavior.rb
@@ -2,7 +2,7 @@ module MacroDeck
 	# Renders a description as a paragraph
 	class DescriptionBehavior < Behavior
 		def to_html
-			"<p>#{@data_object.description}</p>"
+			"<p>#{Rack::Utils.escape_html(@data_object.description)}</p>"
 		end
 	end
 end
