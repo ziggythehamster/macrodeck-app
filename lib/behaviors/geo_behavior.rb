26f11a5674f5f416c75ac76a83cb05881f831269
Keith Gable <ziggy@ignition-project.com>
2011-03-14 21:59:36 -0500

    Use zoom level 15 and mid-sized markers

--- lib/behaviors/geo_behavior.rb
+++ lib/behaviors/geo_behavior.rb
@@ -2,7 +2,7 @@ module MacroDeck
 	# Renders a geolocation as a Google Maps static map
 	class GeoBehavior < Behavior
 		def to_html
-			"<p><img src=\"http://maps.google.com/maps/api/staticmap?size=300x300&amp;markers=#{Rack::Utils.escape_html(@data_object.lat)},#{Rack::Utils.escape_html(@data_object.lng)}&amp;maptype=roadmap&amp;sensor=false\" alt=\"Map of #{Rack::Utils.escape_html(@data_object.title)}\" /></p>"
+			"<p><img src=\"http://maps.google.com/maps/api/staticmap?size=300x300&amp;zoom=15&amp;markers=size:mid%7C#{Rack::Utils.escape_html(@data_object.lat)},#{Rack::Utils.escape_html(@data_object.lng)}&amp;maptype=roadmap&amp;sensor=false\" alt=\"Map of #{Rack::Utils.escape_html(@data_object.title)}\" /></p>"
 		end
 	end
 end
