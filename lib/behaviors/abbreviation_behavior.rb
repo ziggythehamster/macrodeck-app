0c5ac4064163bc3c1a0360e529b56aafe3ea0d9c
Keith Gable <ziggy@ignition-project.com>
2011-03-21 22:32:41 -0500

    Address behavior update for form fields

--- lib/behaviors/abbreviation_behavior.rb
+++ lib/behaviors/abbreviation_behavior.rb
@@ -1,6 +1,12 @@
 module MacroDeck
 	# An abbreviation should not be outputted (this is the default Behavior)
-	# It exists in the title.
+	# It exists in the title. But it should be editable.
 	class AbbreviationBehavior < Behavior
+		def to_form_field
+			output = ""
+			output << form_label(:abbreviation)
+			output << form_input(:abbreviation, :text, :size => 3)
+			return output
+		end
 	end
 end
--- lib/behaviors/address_behavior.rb
+++ lib/behaviors/address_behavior.rb
@@ -8,5 +8,12 @@ module MacroDeck
 				return "<p class=\"adr\"><span class=\"street-address\">#{Rack::Utils.escape_html(@data_object.address)}</span></p>"
 			end
 		end
+
+		def to_form_field
+			output = ""
+			output << form_label(:address)
+			output << form_input(:address)
+			return output
+		end
 	end
 end
