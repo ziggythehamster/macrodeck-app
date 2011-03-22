0f9d8a85acd3b9aab1ce082065488cd09c993174
Keith Gable <ziggy@ignition-project.com>
2011-03-21 22:35:28 -0500

    Add a form field function to the description behavior

--- lib/behaviors/description_behavior.rb
+++ lib/behaviors/description_behavior.rb
@@ -4,5 +4,15 @@ module MacroDeck
 		def to_html
 			"<p>#{Rack::Utils.escape_html(@data_object.description)}</p>"
 		end
+
+		# Return a textarea.
+		def to_form_field
+			output = ""
+			output << form_label(:description)
+			output << "<textarea rows=\"3\" name=\"description\">"
+			output << Rack::Utils.escape_html(@data_object.description)
+			output << "</textarea>"
+			return output
+		end
 	end
 end
