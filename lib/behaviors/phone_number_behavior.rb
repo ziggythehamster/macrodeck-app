d0c4c5c0fad2864f49553bf2c6a94e5f29043f21
Keith Gable <ziggy@ignition-project.com>
2011-03-21 22:37:12 -0500

    Add a form field behavior to the phone number behavior

--- lib/behaviors/phone_number_behavior.rb
+++ lib/behaviors/phone_number_behavior.rb
@@ -4,5 +4,12 @@ module MacroDeck
 		def to_html
 			"<p><a href=\"tel:#{Rack::Utils.escape_html(@data_object.phone_number)}\">#{Rack::Utils.escape_html(@data_object.phone_number)}</a></p>"
 		end
+
+		def to_form_field
+			output = ""
+			output << form_label(:phone_number)
+			output << form_input(:phone_number, :tel)
+			return output
+		end
 	end
 end
