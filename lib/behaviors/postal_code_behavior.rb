beb05563a0e113a66584bc7e8bf4e6be19e64198
Keith Gable <ziggy@ignition-project.com>
2011-03-21 22:38:27 -0500

    Add a postal code form field behavior

--- lib/behaviors/postal_code_behavior.rb
+++ lib/behaviors/postal_code_behavior.rb
@@ -1,5 +1,12 @@
 module MacroDeck
 	# Postal codes shouldn't be shown (they're part of addresses)
+	# Except they're perfectly valid to edit.
 	class PostalCodeBehavior < Behavior
+		def to_form_field
+			output = ""
+			output << form_label(:postal_code)
+			output << form_input(:postal_code, :text, { :size => 10 })
+			return output
+		end
 	end
 end
