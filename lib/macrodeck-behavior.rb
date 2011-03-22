871786cb5ba062012d6ef8519e9306947e8c1c44
Keith Gable <ziggy@ignition-project.com>
2011-03-21 22:11:10 -0500

    Add some private methods to assist in generating form fields

--- lib/macrodeck-behavior.rb
+++ lib/macrodeck-behavior.rb
@@ -9,5 +9,25 @@ module MacroDeck
 		def to_html
 			""
 		end
+
+		# Default form field method. Override this in subclasses.
+		# You probably want something like form_label(:whatever)
+		# combined with form_input(:whatever).
+		def to_form_field
+			""
+		end
+
+
+		private
+			# Function to generate a form input tag.
+			def form_input(field, type = "text")
+				return "<input type=\"#{type}\" name=\"#{field.to_s}\" value=\"#{Rack::Utils.escape_html(@data_object.send(field.to_sym)}\" />"
+			end
+
+			# Function to generate a form label. Can probably be modified to accept a block,
+			# in which case we should include the input inside the label tag.
+			def form_label(field)
+				return "<label for=\"#{field.to_s}\">#{Rack::Utils.escape_html(@data_object.class.human_attribute_name(field.to_sym))}</label>"
+			end
 	end
 end
