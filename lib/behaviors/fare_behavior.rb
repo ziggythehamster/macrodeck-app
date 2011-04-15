458429e93691a8b4e961d247a3eb702b85781fd0
Keith Gable <ziggy@ignition-project.com>
2011-04-15 13:33:21 -0500

    Show form fields for fare.

--- lib/behaviors/fare_behavior.rb
+++ lib/behaviors/fare_behavior.rb
@@ -1,6 +1,12 @@
 module MacroDeck
 	# Renders a list of fares as a list of items, concatenated by commas.
 	class FareBehavior < Behavior
+		def to_form_field
+			out  = form_label(:fare)
+			out << form_input(:fare)
+			return out
+		end
+
 		def to_html
 			Rack::Utils.escape_html(@data_object.fare.join(", "))
 		end
