2edf9fa5924541d48145c5cf44186c262d7cce76
Keith Gable <ziggy@ignition-project.com>
2011-05-03 23:30:17 -0500

    Add Start Time behavior.

---
+++ lib/behaviors/start_time_behavior.rb
@@ -0,0 +1,15 @@
+module MacroDeck
+	# Start Time is the time that the event would start.
+	class StartTimeBehavior < Behavior
+		def to_html
+			return "<abbr title=\"#{@data_object.start_time}\" class=\"dtstart\">#{Time.parse(@data_object.start_time).getlocal.strftime("%b %e @ %l%P")}</abbr>"
+		end
+
+		def to_form_field
+			output = ""
+			output << form_label(:start_time)
+			output << form_input(:start_time, :text)
+			return output
+		end
+	end
+end
