a5163a48e3037fdff9380e2a0127eedc545a18ca
Keith Gable <ziggy@ignition-project.com>
2011-08-02 23:05:14 -0500

    Support hashes being sent to the attribute assignment functions. Sort of.

--- lib/macrodeck-app.rb
+++ lib/macrodeck-app.rb
@@ -132,7 +132,28 @@ module MacroDeck
 				@item = @object.new
 
 				@object.properties.each do |f|
-					@item[f.name.to_sym] = params[f.name.to_sym] unless params[f.name.to_sym].nil?
+					unless params[f.name.to_sym].nil?
+						# Simple types can just be set, but more complicated things
+						# will use the behavior system to parse them.
+						if params[f.name.to_sym].is_a?(Hash)
+							behavior_class = "#{f.to_s}_behavior".camelize
+							behavior_class = "MacroDeck::#{behavior_class}"
+
+							begin
+								behavior = behavior_class.constantize
+							rescue NameError
+								behavior = nil
+							end
+
+							if !behavior.nil? && !(@object.introspections[f.to_sym][:internal] == true) && behavior.respond_to?(:parse_result)
+								@item[f.name.to_sym] = behavior.parse_result(params[f.name.to_sym])
+							else
+								@item[f.name.to_sym] = params[f.name.to_sym] # current behavior
+							end
+						else
+							@item[f.name.to_sym] = params[f.name.to_sym]
+						end
+					end
 				end
 
 				@item.created_by = "_system/MacroDeckApp"
