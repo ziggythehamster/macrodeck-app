465b13db5c34358d842f773556674a08837877e3
Keith Gable <ziggy@ignition-project.com>
2011-08-05 00:46:43 -0500

    Make start_time and end_time utilize the new behaviors.

--- lib/behaviors/end_time_behavior.rb
+++ lib/behaviors/end_time_behavior.rb
@@ -1,14 +1,16 @@
 module MacroDeck
 	# End Time is the time that the event would end.
-	class EndTimeBehavior < Behavior
+	class EndTimeBehavior < TimeBehavior
 		def to_html
 			return "&mdash; <abbr title=\"#{@data_object.end_time}\" class=\"dtend\">#{Time.parse(@data_object.end_time).getlocal.strftime("%l%P")}</abbr>"
 		end
 
 		def to_form_field
 			output = ""
-			output << form_label(:end_time)
-			output << form_input(:end_time, :text)
+			output << "<label for=\"end_time_date\">Date</label>"
+			output << date_picker_field("end_time", @data_object.end_time.strftime("%F"))
+			output << "<label for=\"end_time_time\">Time</label>"
+			output << time_picker_field("end_time", @data_object.end_time.strftime("%H:%M"))
 			return output
 		end
 	end
--- lib/behaviors/start_time_behavior.rb
+++ lib/behaviors/start_time_behavior.rb
@@ -1,14 +1,16 @@
 module MacroDeck
 	# Start Time is the time that the event would start.
-	class StartTimeBehavior < Behavior
+	class StartTimeBehavior < TimeBehavior
 		def to_html
 			return "<abbr title=\"#{@data_object.start_time}\" class=\"dtstart\">#{Time.parse(@data_object.start_time).getlocal.strftime("%b %e, %Y @ %l%P")}</abbr>"
 		end
 
 		def to_form_field
 			output = ""
-			output << form_label(:start_time)
-			output << form_input(:start_time, :text)
+			output << "<label for=\"start_time_date\">Date</label>"
+			output << date_picker_field("start_time", @data_object.start_time.strftime("%F"))
+			output << "<label for=\"start_time_time\">Time</label>"
+			output << time_picker_field("start_time", @data_object.start_time.strftime("%H:%M"))
 			return output
 		end
 	end
