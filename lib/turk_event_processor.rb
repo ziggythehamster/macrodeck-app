a334463340feace52e5cb90c200b79a7959a52ad
Keith Gable <ziggy@ignition-project.com>
2011-12-05 23:13:23 -0600

    Start getting the wheels rolling on validating HITs. HITs aren't posted yet as I've yet to determine how to fire off the initial one. Probably needs to be in Platform, May be best to have a database view to get just the objects in need of being pushed out to MTurk.

--- lib/turk_event_processor.rb
+++ lib/turk_event_processor.rb
@@ -1,5 +1,6 @@
 module MacroDeck
 	class TurkEventProcessor
+		attr_reader :configuration
 		attr_reader :event_type
 		attr_reader :event_time
 		attr_reader :hit_type
@@ -22,6 +23,7 @@ module MacroDeck
 			@hit_type = params[:hit_type]
 			@hit_id = params[:hit_id]
 			@assignment_id = params[:assignment_id]
+			@configuration = params[:configuration]
 		end
 
 		# Cause this event to process.
@@ -36,6 +38,56 @@ module MacroDeck
 		private
 			# To process when a HIT is reviewable.
 			def hit_reviewable
+				if !@hit_id.nil?
+					# Check which HIT type we're checking for here.
+					if @hit_type == @configuration.turk_answer_hit_type_id
+						# Look up the HIT
+						@hit = RTurk::Hit.find(@hit_id)
+
+						# Mark HIT as reviewing.
+						@hit.set_as_reviewing!
+
+						# Parse the JSON stored in annotation
+						begin
+							annotation = JSON.parse(@hit.annotation)
+						rescue JSON::ParserError
+							annotation = {}
+						end
+
+						# Get answers in need of review.
+						@hit.assignments.each do |assignment|
+							if !assignment.approved? && !assignment.rejected?
+								# Create verification HIT.
+								verify_hit = RTurk::Hit.create do |h|
+									h.hit_type_id = @configuration.turk_verify_hit_type_id
+									h.assignments = 1
+									h.lifetime = 604800
+									h.note = { "item_id" => annotation["item_id"], "answer_hit_id" => @hit_id, "answer_assignment_id" => @assignment_id }.to_json
+									# TODO: set question up
+								end
+							end
+						end
+					elsif @hit_type == @configuration.turk_verify_hit_type_id
+						# Look up the HIT
+						@hit = RTurk::Hit.find(@hit_id)
+
+						# Get number of true answers.
+						# Get number of false answers.
+						# If true_count > false_count:
+						# 	Accept original assignment.
+						# 	Accept true answers.
+						# 	Reject false answers.
+						# 	Save the answer to the object.
+						# If false_count > true_count:
+						# 	Reject original assignment.
+						# 	Reject true answers.
+						# 	Accept false answers.
+						# 	Extend the original HIT to allow another answer.
+					else
+						puts "HIT Type ID #{@hit_type} not one of the configured HIT Types!"
+						return false
+					end
+				end
 			end
 	end
 end
