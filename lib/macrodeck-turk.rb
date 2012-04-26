70a207576cfe61cb869af6e64f5990c3a23e4d33
Keith Gable <ziggy@ignition-project.com>
2012-04-25 22:44:51 -0500

    Remove validation path because MTurk does this now.

--- lib/macrodeck-turk.rb
+++ lib/macrodeck-turk.rb
@@ -86,51 +86,5 @@ module MacroDeck
 				erb :"turk_no_questions.html", :layout => self.configuration.layout.to_sym
 			end
 		end
-
-		# Render verification for a question.
-		get '/:id/verify/:answer_hit_id/?' do
-			obj = ::DataObject.get(params[:id])
-
-			if obj.class.respond_to?(:turk_tasks) && !obj.class.turk_tasks.nil? && obj.class.turk_tasks.length > 0
-				# Get the HIT that we're currently working on.
-				hit = RTurk::Hit.find(params[:hitId])
-				begin
-					hit_annotation = JSON.parse(hit.annotation)
-				rescue
-					hit_annotation = {}
-				end
-
-				# Get the answer HIT.
-				answer_hit = RTurk::Hit.find(params[:answer_hit_id])
-				begin
-					answer_annotation = JSON.parse(answer_hit.annotation)
-				rescue
-					answer_annotation = {}
-				end
-
-				# Get the turk task for the answer.
-				task_id = answer_annotation["path"].split("/").last
-				task_id = task_id.split("=")[0] if task_id.include?("=")
-				task = obj.class.turk_task_by_id(task_id)
-
-				# TODO: Traverse the path and the answer tree to get the $$Whatever$$ to replace in the task title.
-
-				# Get the answer assignment from the HIT we're currently working on.
-				answer_assignment = answer_hit.assignments.select do |assignment|
-					assignment.id == hit_annotation["answer_assignment_id"]
-				end.first
-
-				# Get the answer from the HIT.
-				if answer_assignment.answers.key?("answer[]")
-					answer = answer_assignment.answers["answer[]"].split("|")
-				else
-					answer = answer_assignment.answers["answer"]
-				end
-
-				erb :"turk_validation.html", :layout => self.configuration.layout.to_sym, :locals => { :answer => answer, :task => task, :item => obj, :assignment_id => params[:assignmentId] } 
-			else
-				erb :"turk_no_questions.html", :layout => self.configuration.layout.to_sym
-			end
-		end
 	end
 end
