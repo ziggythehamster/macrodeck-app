16727539c93bdad82f6f6054371fbff361c48f37
Keith Gable <ziggy@ignition-project.com>
2012-05-20 23:40:17 -0500

    Convert PhoneNumberBehavior to use two parameters.

--- lib/behaviors/phone_number_behavior.rb
+++ lib/behaviors/phone_number_behavior.rb
@@ -5,10 +5,16 @@ module MacroDeck
 			"<p><a href=\"tel:#{Rack::Utils.escape_html(@data_object.phone_number)}\">#{Rack::Utils.escape_html(@data_object.phone_number)}</a></p>"
 		end
 
-		def to_form_field
+		def to_form_field(field_name = :phone_number, params = {})
+			if params[:name].nil?
+				name = field_name
+			else
+				name = params[:name]
+			end
+
 			output = ""
-			output << form_label(:phone_number)
-			output << form_input(:phone_number, :tel)
+			output << form_label(name)
+			output << form_input(name, :tel)
 			return output
 		end
 	end
