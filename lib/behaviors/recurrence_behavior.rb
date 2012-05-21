c571e93a89028379e21a21ce20c993340dc23ab1
Keith Gable <ziggy@ignition-project.com>
2012-05-21 00:34:03 -0500

    Convert RecurrenceBehavior to use two parameters.

--- lib/behaviors/recurrence_behavior.rb
+++ lib/behaviors/recurrence_behavior.rb
@@ -1,9 +1,15 @@
 module MacroDeck
 	# Renders the recurrence (hidden from HTML, but appears in a form)
 	class RecurrenceBehavior < Behavior
-		def to_form_field
-			out  = form_label(:recurrence)
-			out << "<select name=\"recurrence\">"
+		def to_form_field(field_name = :recurrence, params = {})
+			if params[:name].nil?
+				name = field_name
+			else
+				name = params[:name]
+			end
+
+			out  = form_label(name)
+			out << "<select name=\"#{Rack::Utils.escape_html(name.to_s)}\">"
 			out << "<option value=\"none\">None</option>"
 			out << "<option value=\"weekly\">Weekly</option>"
 			out << "<option value=\"monthly\">Monthly (same day of month)</option>"
