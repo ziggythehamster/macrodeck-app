0e524ddda23f82d642b426190f2f2e56d82df90c
Keith Gable <ziggy@ignition-project.com>
2012-05-21 00:15:21 -0500

    Convert PostalCodeBehavior to use two parameters.

--- lib/behaviors/postal_code_behavior.rb
+++ lib/behaviors/postal_code_behavior.rb
@@ -2,10 +2,16 @@ module MacroDeck
 	# Postal codes shouldn't be shown (they're part of addresses)
 	# Except they're perfectly valid to edit.
 	class PostalCodeBehavior < Behavior
-		def to_form_field
+		def to_form_field(field_name = :postal_code, params = {})
+			if params[:name].nil?
+				name = field_name
+			else
+				name = params[:name]
+			end
+
 			output = ""
-			output << form_label(:postal_code)
-			output << form_input(:postal_code, :text, { :size => 10 })
+			output << form_label(name)
+			output << form_input(name, :text, { :size => 10 })
 			return output
 		end
 	end
