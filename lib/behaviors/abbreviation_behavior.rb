2ea5ee132aad2eb97ad0190f283b6388d92f0fb3
Keith Gable <ziggy@ignition-project.com>
2012-05-21 21:27:59 -0500

    Convert AbbreviationBehavior to use two parameters.

--- lib/behaviors/abbreviation_behavior.rb
+++ lib/behaviors/abbreviation_behavior.rb
@@ -2,10 +2,16 @@ module MacroDeck
 	# An abbreviation should not be outputted (this is the default Behavior)
 	# It exists in the title. But it should be editable.
 	class AbbreviationBehavior < Behavior
-		def to_form_field
+		def to_form_field(field_name = :abbreviation, params = {})
+			if params[:name].nil?
+				name = field_name
+			else
+				name = params[:name]
+			end
+
 			output = ""
-			output << form_label(:abbreviation)
-			output << form_input(:abbreviation, :text, { :size => 3 })
+			output << form_label(name)
+			output << form_input(name, :text, { :size => 3 })
 			return output
 		end
 	end
