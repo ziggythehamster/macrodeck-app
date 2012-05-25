e7eb5f83d9717f1610ee092095f6c8076123b0d1
Keith Gable <ziggy@ignition-project.com>
2012-05-24 22:03:51 -0500

    Make EndTimeBehavior support having a different field name and HTML name.

--- lib/behaviors/end_time_behavior.rb
+++ lib/behaviors/end_time_behavior.rb
@@ -20,12 +20,12 @@ module MacroDeck
 
 			output = ""
 			output << "<label for=\"#{Rack::Utils.escape_html(name.to_s)}_date\">End Date</label>"
-			output << no_end_picker_field(name, @data_object.end_time.nil?, "This event does not have an end time")
-			output << date_picker_field(name, Time.new.strftime("%F")) if @data_object.end_time.nil?
-			output << date_picker_field(name, Time.parse(@data_object.end_time).getlocal.strftime("%F")) unless @data_object.end_time.nil?
+			output << no_end_picker_field(name, @data_object.send(field_name.to_sym).nil?, "This event does not have an end time")
+			output << date_picker_field(name, Time.new.strftime("%F")) if @data_object.send(field_name.to_sym).nil?
+			output << date_picker_field(name, Time.parse(@data_object.send(field_name.to_sym)).getlocal.strftime("%F")) unless @data_object.send(field_name.to_sym).nil?
 			output << "<label for=\"#{Rack::Utils.escape_html(name.to_s)}_time\">End Time</label>"
-			output << time_picker_field(name, Time.new.strftime("%H:%M")) if @data_object.end_time.nil?
-			output << time_picker_field(name, Time.parse(@data_object.end_time).getlocal.strftime("%H:%M")) unless @data_object.end_time.nil?
+			output << time_picker_field(name, Time.new.strftime("%H:%M")) if @data_object.send(field_name.to_sym).nil?
+			output << time_picker_field(name, Time.parse(@data_object.send(field_name.to_sym)).getlocal.strftime("%H:%M")) unless @data_object.send(field_name.to_sym).nil?
 			return output
 		end
 	end
