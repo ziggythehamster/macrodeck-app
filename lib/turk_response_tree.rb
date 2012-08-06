22f2a4e1d0c5957ba1ff9a2106f48d1708d109f2
Keith Gable <ziggy@ignition-project.com>
2012-08-06 01:25:25 -0500

    Properly normalize the path when getting the value. Return the value OUTSIDE of the loop instead of inside like a bonehead. Add some logging.

--- lib/turk_response_tree.rb
+++ lib/turk_response_tree.rb
@@ -58,20 +58,28 @@ module MacroDeck
 			# Similar to +at_path+, except that it returns the value at the path
 			# requested, and can't be used to further traverse the tree.
 			def value_at_path(path)
-				return @values_at_paths[path] if @values_at_paths.key?(path)
+				if path =~ %r{^/}
+					normalized_path = path
+				else
+					normalized_path = "/#{path}"
+				end
+
+				puts "[MacroDeck::TurkResponseTree::Tree] Getting value at path: #{normalized_path}"
+
+				return @values_at_paths[normalized_path] if @values_at_paths.key?(normalized_path)
 
 				# Look up!
-				if path =~ %r{^/}
-					path_components = path.split("/")[1..-1]
-				else
-					path_components = path.split("/")
-				end
+				path_components = normalized_path.split("/")[1..-1]
 
 				root = @hash
 				val = nil
 				path_components.each do |p|
+					puts "[MacroDeck::TurkResponseTree::Tree] Processing path component: #{p}"
+
 					# See if this is the last path component (we need the value)
 					if p == path_components.last
+						puts "[MacroDeck::TurkResponseTree::Tree] Last path component - getting value"
+
 						if p.include?("=")
 							if !root.nil? && root.key?(p.split("=")[0])
 								root = root[p.split("=")[0]]
@@ -86,6 +94,8 @@ module MacroDeck
 							end
 						end
 					else
+						puts "[MacroDeck::TurkResponseTree::Tree] Not last path component - traversing tree"
+
 						if p.include?("=")
 							if root.key?(p)
 								root = root[p]
@@ -100,11 +110,10 @@ module MacroDeck
 							end
 						end
 					end
-
-					@values_at_paths[path] = root
-					return @values_at_paths[path]
 				end
 
+				@values_at_paths[normalized_path] = root
+				return @values_at_paths[normalized_path]
 			end
 		end
 	end
